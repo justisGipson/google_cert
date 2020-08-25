64c906c01a93b063ce2ce16990461cd755fb7f84
Justis Gipson <justis710@gmail.com>
2020-08-25 11:20:42 -0400

    week 3; in action + links to regex docs

--- python_interacting_with_os/week_three/notes.py
+++ python_interacting_with_os/week_three/notes.py
@@ -137,4 +137,61 @@ print(re.search(r'p?each', 'I like peaches'))  # <re.Match object; span=(7, 12),
 # Escape Characters
 # A pattern that includes a \ could be escaping a special regex character or a special string character
 # Use a \, escape character, to match one of the special characters
+print(re.search(r'.com', 'welcome'))  # <re.Match object; span=(2, 6), match='lcom'>  - needs escape char to match '.'
+print(re.search(r'\.com', 'welcome'))  # None - because no '.com' in the string
+print(re.search(r'\.com', 'mydomain.com'))  # <re.Match object; span=(8, 12), match='.com'>
 
+# Use \w to match any alphanumeric character including letters, numbers, and underscores
+# Use \d to match digits
+# Use \s for matching whitespace characters like space, tab or new line
+# Use \b for word boundaries
+print(re.search(r'\w*', 'This is an example'))  # <re.Match object; span=(0, 4), match='This'>
+print(re.search(r'\w*', 'And_this_is_another'))  # <re.Match object; span=(0, 19), match='And_this_is_another'>
+
+import re
+
+
+def check_character_groups(text):
+  result = re.search(r"^\w+\s", text)
+  return result != None
+
+
+print(check_character_groups("One"))  # False
+print(check_character_groups("123  Ready Set GO"))  # True
+print(check_character_groups("username user_01"))  # True
+print(check_character_groups("shopping_list: milk, bread, eggs."))  # False
+
+# regex101.com
+
+# ==========================================================================================
+
+# Regular Expressions in Action
+print(re.search(r'A.*a', 'Argentina'))  # <re.Match object; span=(0, 9), match='Argentina>
+# "Azerbaijan" returns "Azerbaija" because we did not specify the end
+print(re.search(r'A.*a', 'Azerbaijan'))  # <re.Match object; span=(0, 9), match='Azerbaija'>
+print(re.search(r'^A.*a$', 'Azerbaijan'))  # None
+print(re.search(r'^A.*a$', 'Australia'))  # <re.Match object; span=(0, 9), match='Australia'>
+
+pattern = r'^[a-zA-z_][a-zA-Z0-9_]*$'
+print(re.search(pattern, 'this_is_a_valid_variable_name'))  # <re.Match objectl span=(0, 30), match='this_is_a_valid_variable_name'
+print(re.search(pattern, 'this is not a valid variable name'))  # None - spaces aren't included in the pattern
+print(re.search(pattern, 'my_variable1'))  # <re.Match object; span=(0, 12), match='my_variable1'>
+print(re.search(pattern, '2my_variable1'))  # None - char classes don't include search to start with a number
+
+import re
+
+
+def check_sentence(text):
+  result = re.search(r"^[A-Z][ |a-z]*[\.?!]", text)
+  return result != None
+
+
+print(check_sentence("Is this is a sentence?"))  # True
+print(check_sentence("is this is a sentence?"))  # False
+print(check_sentence("Hello"))  # False
+print(check_sentence("1-2-3-GO!"))  # False
+print(check_sentence("A star is born."))  # True
+
+# https://docs.python.org/3/howto/regex.html
+# https://docs.python.org/3/library/re.html
+# https://docs.python.org/3/howto/regex.html#greedy-versus-non-greedy
