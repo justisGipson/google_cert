6a8f82d628fe78c776b7db5e123cd4c76c069d03
Justis Gipson <justis710@gmail.com>
2020-08-25 15:09:27 -0400

    week 3 finished. Start week 4

--- python_interacting_with_os/week_three/notes.py
+++ python_interacting_with_os/week_three/notes.py
@@ -195,3 +195,199 @@ print(check_sentence("A star is born."))  # True
 # https://docs.python.org/3/howto/regex.html
 # https://docs.python.org/3/library/re.html
 # https://docs.python.org/3/howto/regex.html#greedy-versus-non-greedy
+
+# ==========================================================================================
+
+# Advanced Regular Expressions
+# Capturing Groups
+# Use parentheses to capture groups which are portions of the pattern that are enclosed in
+# Below line defines two separate groups
+result = re.search(r'(^\w*), (\w*)$', 'Lovelace, Ada')  # <re.Match object; span=(0, 13), match='Lovelace, Ada'>
+print(result)
+
+print(result.groups())  # ('Lovelace', 'Ada')
+print(result[0])  # Lovelace, Ada
+print(result[2])  # Lovelace
+print(result[2])  # Ada
+
+'{} {}'.format(result[2], result[1])  # Ada Lovelace
+
+def rearrange_name(name):
+  result = re.search(r'(^\w*), (\w*)$', name)
+  if result is None:
+    return name
+  return "{} {}".format(result[2], result[1])
+
+# Support middle initials
+import re
+
+
+def rearrange_name(name):
+  result = re.search(r'^(\w*), (\w*.\w?\.?)$',  name)
+  if result == None:
+    return name
+  return '{} {}'.format(result[2], result[1])
+
+
+name=rearrange_name('Kennedy, John F.')
+print(name)
+
+# ==========================================================================================
+
+# More on Repetition Qualifiers
+# Use {}, curly brackets and one or two numbers to specify a range with numeric repetition qualifiers
+print(re.search(r'[a-zA-Z]{5}', 'a ghost'))  # <re.Match object; span=(2, 7), match='ghost'>
+print(re.search(r'[a-zA-Z]{5}', 'a scary ghost appeared'))  # <re.Match object; span=(2, 7), match='scary'>
+print(re.findall(r'[a-zA-Z]{5}', 'a scary ghost appeared'))  # ['scary', 'ghost', 'appea']
+
+# Use \b, which matches word limits at the beginning and end of the pattern, to match full words
+print(re.findall(r'\b[a-zA-Z]{5}\b', 'A scary ghost appeared'))  # ['scary', 'ghost']
+print(re.findall(r'\w{5,10}', 'I really like strawberries'))  # ['really', 'strawberri']
+print(re.findall(r'\w{5,}', 'I really like strawberries'))  # ['really', 'strawberries']
+print(re.search(r's\w{,20}', 'I really like strawberries'))  # <re.Match object; span=(14, 26), match='strawberries'>
+
+
+import re
+
+
+def long_words(text):
+  pattern = r'\w{7,}'
+  result = re.findall(pattern, text)
+  return result
+
+
+print(long_words("I like to drink coffee in the morning."))  # ['morning']
+print(long_words("I also have a taste for hot chocolate in the afternoon."))  # ['chocolate', 'afternoon']
+print(long_words("I never drink tea late at night."))  # []
+
+# ==========================================================================================
+# Extracting PID using regex
+
+import re
+log = 'July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade'
+regex = r'\[(\d+)\]'
+result = re.search((regex, log))
+print(result[1])  # 12345
+
+result = re.search(regex, 'a completely different string that also has numbers [34567]')
+print(result[1])  # 34567
+
+def extract_pid(log_line):
+  regex = r'\[(\d+)\]'
+  result = re.search(regex, log_line)
+  if result is None:
+    return ''
+  return result[1]
+
+
+print(extract_pid(log))  # 12345
+print(extract_pid("99 elephants in a [cage]"))  # ' '
+
+
+import re
+
+
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]\: (\w+)"
+    result = re.search(regex, log_line)
+    if result is None:
+        return None
+    return "{} ({})".format(result[1], result[2])
+
+
+print(extract_pid("July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"))  # 12345 (ERROR)
+print(extract_pid("99 elephants in a [cage]"))  # None
+print(extract_pid("A string that also has numbers [34567] but no uppercase message"))  # None
+print(extract_pid("July 31 08:08:08 mycomputer new_process[67890]: RUNNING Performing backup"))  # 67890 (RUNNING)
+
+# ==========================================================================================
+
+# Splitting and Replacing
+# Split function from the re module works by taking any regular expression as a separator
+# Use capturing parentheses to split list to include the elements that is used to split the values
+print(re.split(r'[.?!]', 'One sentence. Another one? And a last one!'))  # ['One sentence', ' Another one',
+# 'And a last one', '']
+
+print(re.split(r'([.?!])', 'One sentence. Another one? And the last one!'))  # ['One sentence', '.', ' Another one', '?', ' And the last one', '!', '']
+
+# Sub function from the re module is used for creating new strings by substituting all or part of them for a different string
+# It uses regular expressions for both the matching and the replacing
+print(re.sub(r'[\w.%+-]+@[\w.-]+', '[REDACTED]', 'Received an email for go_nuts95@my.example.com'))  #Received an email for [REDACTED]
+
+print(re.sub(r'^([\w .-]*), ([\w .-]*)$', r'\2 \1', 'Lovelace, Ada'))  # Ada Lovelace
+
+re.split(r"the|a", "One sentence. Another one? And the last one!")  # ['One sentence. Ano', 'r one? And ', ' l', 'st one!']
+
+# https://regexcrossword.com/
+
+# ==========================================================================================
+
+import re
+
+
+def transform_record(record):
+  new_record = re.sub(r',(\d{3})', r',+1-\1', record)
+  return new_record
+
+
+print(transform_record("Sabrina Green,802-867-5309,System Administrator"))
+# Sabrina Green,+1-802-867-5309,System Administrator
+print(transform_record("Eli Jones,684-3481127,IT specialist"))
+# Eli Jones,+1-684-3481127,IT specialist
+print(transform_record("Melody Daniels,846-687-7436,Programmer"))
+# Melody Daniels,+1-846-687-7436,Programmer
+print(transform_record("Charlie Rivera,698-746-3357,Web Developer"))
+# Charlie Rivera,+1-698-746-3357,Web Developer
+
+# ==========================================================================================
+import re
+
+
+def multi_vowel_words(text):
+  pattern = r'(\w+[a,e,i,o,u]{3,}\w+)'
+  result = re.findall(pattern, text)
+  return result
+
+
+print(multi_vowel_words("Life is beautiful"))
+# ['beautiful']
+print(multi_vowel_words("Obviously, the queen is courageous and gracious."))
+# ['Obviously', 'queen', 'courageous', 'gracious']
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner."))
+# ['rambunctious', 'quietly', 'delicious']
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)"))
+# ['queue']
+print(multi_vowel_words("Hello world!"))
+# []
+
+# ==========================================================================================
+import re
+
+
+def transform_comments(line_of_code):
+  result = re.sub(r'[\#]{1,}', '//', line_of_code)
+  return result
+
+
+print(transform_comments("### Start of program"))
+# Should be "// Start of program"
+print(transform_comments("  number = 0   ## Initialize the variable"))
+# Should be "  number = 0   // Initialize the variable"
+print(transform_comments("  number += 1   # Increment the variable"))
+# Should be "  number += 1   // Increment the variable"
+print(transform_comments("  return(number)"))
+# Should be "  return(number)"
+
+# ==========================================================================================
+import re
+
+
+def convert_phone_number(phone):
+  result = re.sub(r'\b(\d{3})-(\d{3})-(\d{4})\b', r'(\1) \2-\3', phone)
+  return result
+
+
+print(convert_phone_number("My number is 212-345-9999.")) # My number is (212) 345-9999.
+print(convert_phone_number("Please call 888-555-1234")) # Please call (888) 555-1234
+print(convert_phone_number("123-123-12345")) # 123-123-12345
+print(convert_phone_number("Phone number of Buckingham Palace is +44 303 123 7300")) # Phone number of Buckingham Palace is +44 303 123 7300
