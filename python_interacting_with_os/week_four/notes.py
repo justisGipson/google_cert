b3c2088dae5ac9e41561cf65cf875f3f7ea0cdee
Justis Gipson <justis710@gmail.com>
2020-09-02 16:08:01 -0400

    week 4 finished, week 5 start

--- python_interacting_with_os/week_four/notes.py
+++ python_interacting_with_os/week_four/notes.py
@@ -64,4 +64,116 @@ import os
 
 # Python Subprocesses
 
+import subprocess
+subprocess.run(['date'])  # Wed Sep  2 14:34:53 EDT 2020  CompletedProcess(args=['date'], returncode=0)
+subprocess.run(['sleep', '2'])  # CompletedProcess(args=['sleep', '2'], returncode=0)
 
+# any command following program name are the command line arguments passed
+
+# ==========================================================================================
+
+# Obtaining the output of a system command
+
+# The "host" command converts a host name to an IP address and vice versa
+# Stores the result in a variable by passing the capture_output=True so that the result can be accessed
+
+result = subprocess.run(['host', '8.8.8.8'], capture_output=True)
+print(result.returncode)  # 0
+print(result.stdout)  # b'8.8.8.8.in-addr.arpa domain name pointer dns.google.\n'
+# b' indicates that the output is an array of bytes
+# python isn't aware of what encoding to use, so it represents it as a series of bytes
+
+# Decode function applies an encoding to transform the bytes into a string
+# It uses a UTF-8 encoding by default
+print(result.stdout.decode().split())  # ['8.8.8.8.in-addr.arpa', 'domain', 'name', 'pointer', 'dns.google.']
+
+# Executes the rm command to remove file that doesn't exist
+result = subprocess.run(["rm", "does_not_exist"], capture_output=True)
+print(result.returncode)  # 1
+# stdout prints empty value since there is an error
+print(result.stdout)  # b''
+# stderr prints error value as the value can be accessed through the stderr attribute
+print(result.stderr)  # b"rm: cannot remove 'does_not_exist': No such file or directory\n"
+
+# Good example of how STDOUT and STDERR are different
+
+# ==========================================================================================
+
+# Advances Subprocess Management
+
+# The copy method creates a new dictionary that can be changed as needed without modifying the original environment
+my_env = os.environ.copy()
+
+# The path variable indicates where the operating system will look for the executable programs
+# Joins /opt/myapp and the old value of the path variable to the path separator
+my_env["PATH"] = os.pathsep.join(["/opt/myapp/", my_env["PATH"]])
+
+# Calls the myapp command, setting the end parameter to the new environment
+result = subprocess.run(["myapp"], env=my_env)
+
+# https://docs.python.org/3/library/subprocess.html
+
+# ==========================================================================================
+
+# Filtering Log Files with Regular Expressions
+
+# check_cron.py
+import re
+
+logfile = sys.argv[1]
+with open(logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            # continue keyword tells the loop to go to the next element
+            continue
+        pattern = r"USER \((\w+)\)$"
+        result = re.search(pattern, line)
+        print(result)
+
+# ==========================================================================================
+
+import re
+def show_time_of_pid(line):
+  pattern = r"([a-zA-Z]+ \d+ \d+:\d+:\d+).*\[(\d+)\]\:"
+  result = re.search(pattern, line)
+  return '{} pid:{}'.format(result.group(1), result.group(2))
+
+print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+
+print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
+
+print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
+
+print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+
+print(show_time_of_pid("Jul 6 14:04:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:04:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:05:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:05:01 pid:29440
+
+# ==========================================================================================
+
+usernames = {}
+name = 'good_user'
+usernames[name] = usernames.get(name, 0) + 1
+print(usernames)  # {'good_user': 1}
+
+# ==========================================================================================
+
+import re
+
+logfile = sys.argv[1]
+usernames = {}
+with open(logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            # continue keyword tells the loop to go to the next element
+            continue
+        pattern = r"USER \((\w+)\)$"
+        result = re.search(pattern, line)
+        if result is None:
+            continue
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) + 1
+print(usernames)
